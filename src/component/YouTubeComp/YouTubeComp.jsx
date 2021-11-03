7abb1c88b6432e18b62cf567e6f313517f8567c6
akhba01 <aakhbar21@gmail.com>
2021-11-04 06:15:13 +0700

    2 files modified

--- src/component/YouTubeComp/YouTubeComp.jsx
+++ src/component/YouTubeComp/YouTubeComp.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
-
+import './YouTubeComp.css'
 const YouTubeComp = () =>{
   return (
-    <div>
+    <div className="youtube-wrapper">
+      <div className="img-thum">
+        <img src="" alt=""/>
+      </div>
+      <p className="title">title here</p>
+      <p className="desc">desc here</p>
 
     </div>
   )
